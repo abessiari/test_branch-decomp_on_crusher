35ed987215b54669e82234932c5ef7f5fa9a5e98
Abdelilah Essiari <aessiari@login2.crusher.olcf.ornl.gov>
2022-04-12 15:13:37 -0400

    data for testing branch decomp

---
+++ build_scripts/build-kokkos.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+module load git-lfs
+module load rocm/5.0.2
+module load cmake
+
+hipcc_path=$(which hipcc)
+rocm_path=$(dirname $hipcc_path)/../
+
+home_dir=${PWD}
+
+kokkos_src_dir=${home_dir}/kokkos/src
+kokkos_build_dir=${home_dir}/kokkos/build
+kokkos_install_dir=${home_dir}/kokkos/install
+
+echo $kokkos_src_dir
+echo $kokkos_build_dir
+echo $kokkos_install_dir
+
+git clone https://github.com/kokkos/kokkos.git ${kokkos_src_dir}
+
+cmake -S ${kokkos_src_dir} -B ${kokkos_build_dir} \
+  -DCMAKE_BUILD_TYPE=DEBUG \
+  -DBUILD_SHARED_LIBS=ON\
+  -DKokkos_ARCH_VEGA90A=ON \
+  -DCMAKE_CXX_COMPILER=${rocm_path}/bin/hipcc \
+  -DKokkos_ENABLE_HIP=ON \
+  -DKokkos_ENABLE_SERIAL=ON \
+  -DKokkos_ENABLE_HIP_RELOCATABLE_DEVICE_CODE=OFF \
+  -DCMAKE_INSTALL_PREFIX=${kokkos_install_dir} \
+  -DCRAYPE_LINK_TYPE=dynamic \
+  -DCMAKE_VERBOSE_MAKEFILE=ON \
+  -DCMAKE_CXX_FLAGS_DEBUG="-g -O0" \
+  -DKokkos_ENABLE_EXAMPLES=OFF \
+  -DKokkos_ENABLE_DEBUG=ON \
+  -DKokkos_ENABLE_DEBUG_BOUNDS_CHECK=ON \
+  -DKokkos_ENABLE_DEBUG_DUALVIEW_MODIFY_CHECK=ON \
+  -DCMAKE_CXX_FLAGS="--amdgpu-target=gfx90a"
+
+cmake --build ${kokkos_build_dir} -j10
+cmake --install ${kokkos_build_dir}
---
+++ build_scripts/build-vtkm.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+module load git-lfs
+module load rocm/5.0.2
+module load cmake
+
+hipcc_path=$(which hipcc)
+rocm_path=$(dirname $hipcc_path)/..
+echo "************"
+echo $hipcc_path
+echo $rocm_path
+echo "************"
+
+home_dir=${PWD}
+vtkm_src_dir=${home_dir}/vtkm/src
+vtkm_build_dir=${home_dir}/vtkm/build
+vtkm_install_dir=${home_dir}/vtkm/install
+kokkos_install_dir=${home_dir}/kokkos/install
+
+git clone https://gitlab.kitware.com/ghweber/vtk-m.git -b contour-tree-branch-decomposition ${vtkm_src_dir}
+
+echo cmake -S ${vtkm_src_dir} -B ${vtkm_build_dir} \
+  -DCMAKE_BUILD_TYPE=DEBUG \
+  -DBUILD_SHARED_LIBS=ON\
+  -DVTKm_NO_DEPRECATED_VIRTUAL=ON \
+  -DVTKm_ENABLE_KOKKOS=ON \
+  -DVTKm_ENABLE_RENDERING=OFF \
+  -DVTKm_ENABLE_TESTING=OFF \
+  -DVTKm_ENABLE_BENCHMARKS=OFF \
+  -DVTKm_ENABLE_EXAMPLES=ON \
+  -DVTKM_EXAMPLE_CONTOURTREE_ENABLE_DEBUG_PRINT=OFF \
+  -DCMAKE_HIP_ARCHITECTURES="gfx90a" \
+  -DVTKm_ENABLE_MPI=ON \
+  -DCMAKE_PREFIX_PATH="${kokkos_install_dir}" \
+  -DCMAKE_INSTALL_PREFIX=${vtkm_install_dir} \
+  -DCMAKE_CXX_COMPILER=${rocm_path}/llvm/bin/clang++ \
+  -DCMAKE_VERBOSE_MAKEFILE=ON \
+  -DCMAKE_C_FLAGS_DEBUG="-g -O0" \
+  -DCMAKE_CXX_FLAGS_DEBUG="-g -O0" \
+  -DCMAKE_HIP_FLAGS_DEBUG="-g -O0" \
+  -DCMAKE_HIP_COMPILER_FORCED=TRUE \
+  -DCLANGRT_BUILTINS=${rocm_path}/llvm/lib/clang/14.0.0/lib/linux/libclang_rt.builtins-x86_64.a \
+  -DCMAKE_C_COMPILER=${rocm_path}/llvm/bin/clang
+
+module load rocm/5.0.2
+cmake --build ${vtkm_build_dir} -j10
